68da6e3e4460c69d1299a71d470b645afeed5346
TheEldaps <adejames97@gmail.com>
2025-10-05 03:20:50 +0100

    Style Hero section

--- app/component/Hero.tsx
+++ app/component/Hero.tsx
@@ -4,23 +4,48 @@ import HeroImage from "../../public/HeroImage.png";
 
 export default function Hero() {
   return (
-    <section>
-      <section>
-        <h3>BEST DESTINATIONS AROUND THE WORLD</h3>
+    <section className="flex justify-evenly pt-[30px] mb-[500rem]">
+      <section className="pt-10 max-w-[50%]">
+        <h3 className="text-[#DF6951] text-[1.2rem] font-[poppins] font-bold my-2 mb-5">
+          BEST DESTINATIONS AROUND THE WORLD
+        </h3>
 
-        <h1>Travel, enjoy and live a new and full life</h1>
+        <h1 className="font-[volkhov] text-[#181E4B] text-[5rem] font-bold leading-[80px] tracking-tighter">
+          Travel,
+          <span className="relative inline-block">
+            <span className="relative z-10">enjoy</span>
+            <span
+              aria-hidden="true"
+              className="absolute left-0 right-0 -bottom-1 z-0 w-full h-3 bg-no-repeat bg-center bg-contain bg-[url('../../public/UnderlineDecore.png')]"
+            />
+          </span>{" "}
+          and live a new and full life
+        </h1>
 
-        <p>
+        <p className="mt-[20px] font-[poppins]">
           Built wicket longer admire do barton vanity itself do in it.
           <br /> Prefferred to sportsmen it engrossed listening.Park gate sell
           they west hard for the.
         </p>
 
-        <div>
-          <button>Find out more</button>
-          <Image src={playButton} width="112" height="112" alt="playbutton" />
+        <div className="flex gap-10 items-center mt-[20px]  ">
+          <button className="px-5 py-3 bg-[#F1A501] font-[poppins] rounded-[0.5rem] text-white text-[0.85rem] cursor-pointer">
+            Find out more
+          </button>
+
+          <button className="flex items-center cursor-pointer">
+            <Image
+              src={playButton}
+              width="112"
+              height="112"
+              alt="playbutton"
+              className="w-[60px] h-[60px] mt-[12px]"
+            />
+            <span className="font-[poppins] text-[0.85rem]">Play Demo</span>
+          </button>
         </div>
       </section>
+
       <section>
         <Image
           src={HeroImage}
